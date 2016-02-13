3d509429b8d117a2667795c9dba5c6e53bbcf301
Dan Rammer <hamersaw@gmail.com>
2016-02-12 22:18:12 -0700

    added tcp listener to dht

--- src/dht/dht.go
+++ src/dht/dht.go
@@ -3,6 +3,7 @@ package dht
 import (
 	"errors"
 	"fmt"
+	"net"
 )
 
 type DHTService struct {
@@ -15,12 +16,17 @@ type DHTService struct {
 }
 
 func NewDHTService(tokens []uint64, address string, applicationAddress string, seedAddresses []string) *DHTService {
+	lookupTable := make(map[uint64]string)
+	for _, token := range tokens {
+		lookupTable[token] = applicationAddress
+	}
+
 	return &DHTService {
 		tokens: tokens,
 		address: address,
 		applicationAddress: applicationAddress,
 		seedAddresses: seedAddresses,
-		lookupTable: make(map[uint64]string),
+		lookupTable: lookupTable,
 		peerTable: make(map[string][]uint64),
 	}
 }
@@ -28,15 +34,38 @@ func NewDHTService(tokens []uint64, address string, applicationAddress string, s
 func (d *DHTService) Start() error {
 	fmt.Println("starting dht service")
 
-	//send join messages to seed addresses
+	//send node join messages to all seeds
 	for _, ipAddress := range d.seedAddresses {
-		fmt.Printf("sending join message %v", ipAddress)
+		fmt.Printf("TODO - send join message %v", ipAddress)
+	}
+
+	//start listening for connections
+	listener, err := net.Listen("tcp", d.address)
+	if err != nil {
+		return err
+	}
+
+	for {
+		fmt.Printf("listening for connections on %s\n", d.address)
+		if conn, err := listener.Accept(); err == nil {
+			go d.handleConn(conn)
+		} else {
+			continue
+		}
 	}
 
 	return nil
 }
 
+func (d *DHTService) handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Printf("TODO - handle client connections")
+}
+
 func (d *DHTService) Lookup(token uint64) (string, error) {
+	fmt.Printf("TODO - handle token lookup")
+
 	return "", errors.New("unimplemented")
 }
 
