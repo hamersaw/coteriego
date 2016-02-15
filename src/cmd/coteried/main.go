f74733f3ee54a7b3c0e71d9fb10af206453889da
Dan Rammer <hamersaw@gmail.com>
2016-02-14 21:19:42 -0700

    writing records from coteried to the record store.

--- src/cmd/coteried/main.go
+++ src/cmd/coteried/main.go
@@ -7,6 +7,7 @@ import (
 
 	"coterie"
 	"dht"
+	"recordStore"
 
 	"github.com/BurntSushi/toml"
 )
@@ -38,27 +39,107 @@ func main() {
 		panic(err)
 	}
 
+	recordStore := recordStore.NewRecordStore()
 	for {
 		if conn, err := listener.Accept(); err == nil {
-			go handleConn(conn)
+			go handleConn(conn, recordStore, dhtService)
 		} else {
 			continue
 		}
 	}
 }
 
-func handleConn(conn net.Conn) {
+func handleConn(conn net.Conn, recStore *recordStore.RecordStore, dhtService *dht.DHTService) {
 	defer conn.Close()
-	coterieMsg, err := coterie.ReadCoterieMsg(conn)
-	if err != nil {
-		panic(err)
+	conns := make(map[string]net.Conn)
+
+	for {
+		coterieMsg, err := coterie.ReadCoterieMsg(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		switch(coterieMsg.Type) {
+		case coterie.CoterieMsg_CLOSE_CONNECTION:
+			break
+		case coterie.CoterieMsg_INSERT_ENTRY:
+			insertEntryMsg := coterieMsg.GetInsertEntryMsg()
+			if err := recStore.InsertEntry(insertEntryMsg.Token, insertEntryMsg.Key, insertEntryMsg.Value); err != nil {
+				panic(err)
+			}
+			continue
+		case coterie.CoterieMsg_INSERT_RECORD:
+			insertRecordMsg := coterieMsg.GetInsertRecordMsg()
+			if err := recStore.InsertRecord(insertRecordMsg.Token, insertRecordMsg.GetRecord().GetEntries()); err != nil {
+				panic(err)
+			}
+			continue
+		case coterie.CoterieMsg_RECORD_BATCH:
+			recordBatchMsg := coterieMsg.GetRecordBatchMsg()
+			for _, record := range recordBatchMsg.GetRecords() {
+				recordKey := recordStore.ComputeRecordToken(record.GetEntries())
+				recordAddress, err := dhtService.Lookup(recordKey)
+				if err != nil {
+					panic(err)
+				}
+
+				insertRecordMsg := new(coterie.CoterieMsg)
+				insertRecordMsg.Type = coterie.CoterieMsg_INSERT_RECORD
+				insertRecordMsg.InsertRecordMsg = &coterie.InsertRecordMsg { recordKey, record }
+
+				recordConn := getConn(recordAddress, conns)
+				err = coterie.WriteCoterieMsg(insertRecordMsg, recordConn)
+				if err != nil {
+					panic(err)
+				}
+
+				for key, value := range record.GetEntries() {
+					entryKey := recordStore.ComputeEntryToken(key, value)
+					entryAddress, err := dhtService.Lookup(entryKey)
+					if err != nil {
+						panic(err)
+					}
+
+					insertEntryMsg := new(coterie.CoterieMsg)
+					insertEntryMsg.Type = coterie.CoterieMsg_INSERT_ENTRY
+					insertEntryMsg.InsertEntryMsg = &coterie.InsertEntryMsg { entryKey, key, value }
+
+					entryConn := getConn(entryAddress, conns)
+					err = coterie.WriteCoterieMsg(insertEntryMsg, entryConn)
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+			continue
+		default:
+			fmt.Printf("TODO - handle coterie messsage type: %v\n", coterieMsg.Type)
+			break
+		}
+
+		break
 	}
 
-	switch(coterieMsg.Type) {
-	case coterie.CoterieMsg_INSERT_RECORD:
-		break
-	default:
-		fmt.Printf("TODO - handle coterie messsage type: %v\n", coterieMsg.Type)
-		break
+	closeConnectionMsg := new(coterie.CoterieMsg)
+	closeConnectionMsg.Type = coterie.CoterieMsg_CLOSE_CONNECTION
+	for _, conn := range conns {
+		coterie.WriteCoterieMsg(closeConnectionMsg, conn)
+		conn.Close()
 	}
+}
+
+func getConn(address string, conns map[string]net.Conn) net.Conn {
+	var conn net.Conn
+	if _, ok := conns[address]; ok {
+		conn, _ = conns[address]
+	} else {
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			panic(err)
+		}
+
+		conns[address] = conn
+	}
+
+	return conn
 }
--- src/recordStore/recordStore.go
+++ src/recordStore/recordStore.go
@@ -10,7 +10,14 @@ type RecordStore struct {
 	entries  map[string]map[string][]uint64
 }
 
-func (d *RecordStore) StoreRecord(token uint64, record map[string]string) error {
+func NewRecordStore() *RecordStore {
+	return &RecordStore {
+		make(map[uint64]map[string]string),
+		make(map[string]map[string][]uint64),
+	}
+}
+
+func (d *RecordStore) InsertRecord(token uint64, record map[string]string) error {
 	if _, ok := d.records[token]; ok {
 		return errors.New(fmt.Sprintf("Token %d already exists in records map", token))
 	}
@@ -19,7 +26,7 @@ func (d *RecordStore) StoreRecord(token uint64, record map[string]string) error 
 	return nil
 }
 
-func (d *RecordStore) StoreEntry(token uint64, field string, value string) error {
+func (d *RecordStore) InsertEntry(token uint64, field string, value string) error {
 	m, ok := d.entries[field]
 	if !ok {
 		m = make(map[string][]uint64)
@@ -58,10 +65,10 @@ func (d *RecordStore) GetMatchingRecordKeys(field string, value string, match fu
 	return tokens, nil
 }
 
-func ComputeRecordKey(record map[string]string) uint64 {
+func ComputeRecordToken(record map[string]string) uint64 {
 	return 0
 }
 
-func ComputeEntryKey(key string, value string) uint64 {
+func ComputeEntryToken(key string, value string) uint64 {
 	return 0
 }
